67b88b63541a4faf851a1da3b03291ed6fa32255
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-26 20:36:24 -0300

    Creating component hello

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+function Hello(props) {
+  return <h2>Welcome to React</h2>;
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Bienvenido a React</h2>
+          <Hello />
         </header>
         <h3>Vamos a aprender React</h3>
         <p>Hola mundo, estoy usando React</p>
