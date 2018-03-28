82f32831ecf6ffe97155782c15a5cf120847bc2e
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-28 20:03:26 -0300

    Initializing the state by props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 class Contador extends Component {
-  constructor() {
-    super();
-    this.state = { contador: 1 };
+  constructor(props) {
+    super(props);
+    this.state = { contador: this.props.contadorInicial };
     setInterval(() => {
       this.setState({ contador: this.state.contador + 1 });
     }, 1000);
@@ -13,6 +13,10 @@ class Contador extends Component {
     return <ContadorNumero numero={this.state.contador} />;
   }
 }
+
+Contador.defaultProps = {
+  contadorInicial: 0
+};
 
 class ContadorNumero extends Component {
   render() {
@@ -26,7 +30,7 @@ class App extends Component {
     return (
       <div className="App">
         <p>Primer componente con state</p>
-        <Contador />
+        <Contador contadorInicial={100} />
       </div>
     );
   }
