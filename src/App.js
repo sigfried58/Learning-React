b9ed841abca96896da11ac9f8f2fb08648e70b8d
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-28 19:19:46 -0300

    State II and III- Update state with setState and Propagation of the state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ class Contador extends Component {
   constructor() {
     super();
     this.state = { contador: 1 };
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 });
+    }, 1000);
   }
 
   render() {
-    return <span>{this.state.contador}</span>;
+    return <ContadorNumero numero={this.state.contador} />;
+  }
+}
+
+class ContadorNumero extends Component {
+  render() {
+    console.log("ContadorNumero render()");
+    return <span>{this.props.numero}</span>;
   }
 }
 
