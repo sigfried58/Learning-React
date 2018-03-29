309d3eb0c23a734c8e1670fab5fd73a8b93108fc
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-29 15:32:12 -0300

    working with lists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import ConditionalSection from "./sections/conditional";
 
 class App extends Component {
   render() {
+    const numbers = [1, 1, 3, 4, 5];
     return (
       <div className="App">
-        <ConditionalSection />
+        <h4>Trabajando con listas</h4>
+        {numbers.map((number, index) => {
+          return <p key={index}>Soy el numero {number}</p>;
+        })}
       </div>
     );
   }
