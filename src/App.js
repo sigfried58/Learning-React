f3490f37eb27541f00cde4786be65a0a160be138
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-29 08:37:05 -0300

    Conditional on the render method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from "react";
-
-class Contador extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { contador: this.props.contadorInicial };
-    setInterval(() => {
-      this.setState({ contador: this.state.contador + 1 });
-    }, 1000);
-  }
-
-  render() {
-    return <ContadorNumero numero={this.state.contador} />;
-  }
-}
-
-Contador.defaultProps = {
-  contadorInicial: 0
-};
-
-class ContadorNumero extends Component {
-  render() {
-    console.log("ContadorNumero render()");
-    return <span>{this.props.numero}</span>;
-  }
-}
+import ConditionalSection from "./sections/conditional";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Primer componente con state</p>
-        <Contador contadorInicial={100} />
+        <ConditionalSection />
       </div>
     );
   }
