481f2214cacf74eb8987088c0b5b740b13c2b2a8
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-29 20:38:00 -0300

    Synthetic events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  handleClick(e) {
+    console.log(e);
+    console.log(e.nativeEvent);
+    alert("Hi here");
+  }
+
   render() {
     return (
       <div className="App">
         <h4>Eventos</h4>
-        <button onClick={() => alert("Hi There!")}>Hi There!</button>
+        <button onClick={this.handleClick}>Hi There!</button>
       </div>
     );
   }
