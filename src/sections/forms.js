bc7564c7b60057a6bbdba117b22cc3785f339bf1
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-04-04 23:57:16 -0300

    Forms in React

---
+++ src/sections/forms.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Forms extends Component {
+  handleClick(e) {
+    e.preventDefault(); //Para evitar que el boton haga submit
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('twitter').value;
+    console.log({ name, email });
+  }
+  render() {
+    return (
+      <div>
+        <h4>Formularios</h4>
+        <form>
+          <p>
+            <label>Nombre:</label>
+            <input
+              id="name"
+              name="userName"
+              placeholder="Introduce el nombre"
+            />
+          </p>
+
+          <p>
+            <label>Twitter:</label>
+            <input
+              id="twitter"
+              name="twitterAccount"
+              placeholder="Introduce tu Twitter"
+            />
+          </p>
+          <button onClick={this.handleClick}>Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
