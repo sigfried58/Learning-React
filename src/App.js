38eedfa8fd5a6b254f38542ba7d06072a914fb53
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-04-25 15:40:42 -0300

    children layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import Forms from './sections/forms';
 class Box extends Component {
   render() {
     return (
-      <div style={{ border: '1px solid #09f', margin: 5, padding: 5 }}>
+      <div style={{ border: '1px solid #000', margin: 5, padding: 5 }}>
         {this.props.children}
       </div>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p>
+          <em>Escrito por {this.props.author}</em>
+        </p>
+        <Box>{this.props.date}</Box>
+        <article>{this.props.children}</article>
+      </section>
     );
   }
 }
@@ -15,8 +30,19 @@ class App extends Component {
     return (
       <div className="App">
         <h4>Children Props</h4>
-        <Box>Hola! Soy un children!</Box>
-        <Box>Otro Box con otro contenido</Box>
+        <Article
+          author="Jimmy"
+          date={new Date().toLocaleDateString()}
+          title="Artículo sobre la prop children"
+        >
+          <p>
+            El contenido que envolvemos dentro del componente Article será
+            enviado al componente como this.props.children
+          </p>
+          <strong>
+            Y mantiene las etiquetas y componentes que hayáis añadido dentro
+          </strong>
+        </Article>
       </div>
     );
   }
