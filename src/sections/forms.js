4cbf937db6221c4c71dbbb424485aef89c28174e
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-04-25 12:36:06 -0300

    Events onSubmit and onChange

--- src/sections/forms.js
+++ src/sections/forms.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Forms extends Component {
-  handleClick = e => {
+  handleSubmit = e => {
     e.preventDefault(); //Para evitar que el boton haga submit
     // const name = document.getElementById('name').value;
     const name = this.inputName.value;
     const email = document.getElementById('twitter').value;
     console.log({ name, email });
   };
+
+  handleChange(e) {
+    console.log('handleChange');
+    console.log(e.target.checked);
+  }
+
   render() {
     return (
       <div>
         <h4>Formularios</h4>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>
             <label htmlFor="name">Nombre:</label>
             <input
@@ -31,7 +37,14 @@ export default class Forms extends Component {
               placeholder="Introduce tu Twitter"
             />
           </p>
-          <button onClick={this.handleClick}>Enviar</button>
+
+          <p>
+            <label>
+              <input onChange={this.handleChange} type="checkbox" />
+              Accepted terms
+            </label>
+          </p>
+          <button>Enviar</button>
         </form>
       </div>
     );
